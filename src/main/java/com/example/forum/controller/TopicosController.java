b361039ca321cd5eee7744cf0960735b52573fbe
andressamesquita <rib.mesq.andressa72@gmail.com>
2021-03-27 00:01:04 -0300

    .

--- src/main/java/com/example/forum/controller/TopicosController.java
+++ src/main/java/com/example/forum/controller/TopicosController.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.forum.controller.dto.TopicoDto;
@@ -11,9 +12,9 @@ import com.example.forum.modelo.Curso;
 import com.example.forum.modelo.Topico;
 
 @RestController
-@RequestMapping("/topicos")
 public class TopicosController {
 	
+	@RequestMapping("/topicos")
 	public List<TopicoDto> lista(){
 		Topico topico = new Topico("Dúvida", "Dúvida com Spring", new Curso("Spring", "Programação"));
 		
