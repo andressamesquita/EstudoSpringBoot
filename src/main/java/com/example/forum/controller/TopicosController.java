881cad438168d2d52046cbf55910253f8f3e1db1
andressamesquita <rib.mesq.andressa72@gmail.com>
2021-03-27 23:50:35 -0300

    Refatoração TopicosController, implementação do método cadastrar

--- src/main/java/com/example/forum/controller/TopicosController.java
+++ src/main/java/com/example/forum/controller/TopicosController.java
@@ -3,30 +3,43 @@ package com.example.forum.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.forum.controller.dto.TopicoDto;
+import com.example.forum.controller.form.TopicoForm;
 import com.example.forum.modelo.Topico;
+import com.example.forum.repository.CursoRepository;
 import com.example.forum.repository.TopicoRepository;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 	
 	@Autowired
 	private TopicoRepository topicoRepository;
 	
-	@RequestMapping("/topicos")
+	@Autowired
+	private CursoRepository cursoRepository;
+	
+	@GetMapping
 	public List<TopicoDto> lista(String nomeCurso){
 		if (nomeCurso == null) {
 			List<Topico> topicos = topicoRepository.findAll();
 			return TopicoDto.converter(topicos);
 		}else {
-			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);;
+			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
 			return TopicoDto.converter(topicos);
 		}
-		
-		
+	}
+	
+	@PostMapping
+	public void cadastrar(@RequestBody TopicoForm form) {
+		Topico topico = form.converter(cursoRepository);
+		topicoRepository.save(topico);
 	}
 
 }
