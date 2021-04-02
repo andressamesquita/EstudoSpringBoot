1e3b7c88305b5cd2f9b0e1da1da6367f59d6746b
andressamesquita <rib.mesq.andressa72@gmail.com>
2021-04-02 13:52:37 -0300

    Gerando token com JWT

--- src/main/java/com/example/forum/controller/AutenticacaoController.java
+++ src/main/java/com/example/forum/controller/AutenticacaoController.java
@@ -2,23 +2,42 @@ package com.example.forum.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.forum.config.security.TokenService;
 import com.example.forum.controller.form.LoginForm;
 
 @RestController
 @RequestMapping("/auth")
 public class AutenticacaoController {
+	
+	@Autowired
+	AuthenticationManager authManager;
+	
+	@Autowired
+	private TokenService tokenService;
 
 	@PostMapping
 	public ResponseEntity<?> autenticar(@RequestBody @Valid LoginForm form){
-		System.out.println(form.getEmail());
-		System.out.println(form.getSenha());
+		UsernamePasswordAuthenticationToken dadosLogin = form.converter();
 		
-		return ResponseEntity.ok().build();
+		try {
+			Authentication authentication = authManager.authenticate(dadosLogin);
+			String token = tokenService.gerarToken(authentication);
+			System.out.println(token);
+			return ResponseEntity.ok().build();
+			
+		} catch (org.springframework.security.core.AuthenticationException e) {
+			return ResponseEntity.badRequest().build();
+			
+		}
 	}
 }
--- src/main/java/com/example/forum/controller/form/LoginForm.java
+++ src/main/java/com/example/forum/controller/form/LoginForm.java
@@ -1,5 +1,7 @@
 package com.example.forum.controller.form;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 public class LoginForm {
 	
 	private String email;
@@ -17,6 +19,10 @@ public class LoginForm {
 	}
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	
+	public UsernamePasswordAuthenticationToken converter() {
+		return new UsernamePasswordAuthenticationToken(email, senha);
 	}
 	
 	
