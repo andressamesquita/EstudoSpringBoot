0ba916a18e851f6c492a0d07cbd550b6f779cb57
andressamesquita <rib.mesq.andressa72@gmail.com>
2021-04-05 16:34:05 -0300

    Controle de autorização baseado em perfil

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,11 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$BVMut94AaWA2XKz75z0GAOy0raJq1EPNyJ.PB/4pY0twnpcYLHptK');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$BVMut94AaWA2XKz75z0GAOy0raJq1EPNyJ.PB/4pY0twnpcYLHptK');
+
+INSERT INTO PERFIL(id, nome) values ('1', 'ROLE_ALUNO');
+INSERT INTO PERFIL(id, nome) values ('2', 'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS (usuario_id, perfis_id) VALUES (1, 1);
+INSERT INTO USUARIO_PERFIS (usuario_id, perfis_id) VALUES (2, 2);
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
