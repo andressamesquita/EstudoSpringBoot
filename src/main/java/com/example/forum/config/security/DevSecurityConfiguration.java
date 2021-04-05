5f3ac9291109fc128c9a20fd3aee0000e869bb9f
andressamesquita <rib.mesq.andressa72@gmail.com>
2021-04-05 18:25:24 -0300

    Refatoração de código da DevSecurityConfiguration

--- src/main/java/com/example/forum/config/security/DevSecurityConfiguration.java
+++ src/main/java/com/example/forum/config/security/DevSecurityConfiguration.java
@@ -1,69 +1,20 @@
 package com.example.forum.config.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.example.forum.repository.UsuarioRepository;
 
 @EnableWebSecurity
 @Configuration
 @Profile("dev")
-public class DevSecurityConfiguration extends WebSecurityConfigurerAdapter{
+public class DevSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	private AutenticacaoService autenticacaoService;
-	
-	@Autowired
-	private TokenService tokenService;
-	
-	@Autowired
-	private UsuarioRepository usuarioRepository;
-	
-	@Override
-	@Bean
-	protected AuthenticationManager authenticationManager() throws Exception {
-		return super.authenticationManager();
-	}
-	
-	//Configuracoes de autenticacao
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
-	}
-	
-	//Configurcoes de Autorizacao
+	// Configurcoes de Autorizacao
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
-		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
-		.antMatchers(HttpMethod.POST, "/auth").permitAll()
-		.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-		.antMatchers(HttpMethod.DELETE, "/topicos/*").hasRole("MODERADOR")
-		.anyRequest().authenticated()
-		.and().csrf().disable()
-		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and()
-		.addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
-	
+		http.authorizeRequests().antMatchers("/**").permitAll().and().csrf().disable();
 	}
-	
-	//Configuracoes de recursos estaticos(js, css, imagens, etc)
-	@Override
-	public void configure(WebSecurity web) throws Exception {
 
-	}
-	
 }
